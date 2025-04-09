2fabea8f703855d585bb1a3a6710b9e03b3d3fe8
RiverWhite00189 <aparyani17@gmail.com>
2025-04-09 10:37:47 -0500

    challenge 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof start !== "number" || typeof end !== "number") {
+        return "ERROR";
+    } else if (start < 0 || end < 0) {
+        return "ERROR";
+    } else if (Math.floor(start) !== start || Math.floor(end) !== end) {
+        return "ERROR";
+    }
+    let sum = 0;
+    if (start <= end) {
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = end; i <= start; i++) {
+            sum += i;
+        }
+    }
 
+    return sum;
 };
 
 // Do not edit below this line

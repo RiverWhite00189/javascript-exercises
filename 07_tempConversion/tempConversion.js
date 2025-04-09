d9a051d00f0184e9722e6c39096b9ffbdd56af39
RiverWhite00189 <aparyani17@gmail.com>
2025-04-09 12:55:40 -0500

    challenge 6 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let c = (temp - 32) * (5/9)
+  if (c == 0) return c;
+  return parseFloat(c.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let f = (temp * (9/5)) + 32;
+  if (f == 0) return f;
+  return parseFloat(f.toFixed(1));
 };
 
 // Do not edit below this line

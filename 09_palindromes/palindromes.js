dcef1932ccecc7c85110d6e736777c1bdddc2f99
RiverWhite00189 <aparyani17@gmail.com>
2025-04-09 14:47:31 -0500

    09 WIP

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let strArr = []; 
+    for (let i = 0; i < str.length; i++) {
+        if (isLetter(str.at(i))) {     
+            strArr.push(str.at(i)); 
+        }
+    }
 
+    str = strArr.join("");
+    let reverse  = str.split("").reverse().join("");
+    return str == reverse;
 };
 
 // Do not edit below this line
